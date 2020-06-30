8ac799d768fe167649429c7faf3bddc032a28694
Ben <bendotbike@gmail.com>
2020-06-30 14:28:30 -0400

    Removed electron boilerplate, made code more node-centric

--- node/run_shell_command.js
+++ node/run_shell_command.js
@@ -1,23 +1,25 @@
 // Runs a shell command and outputs results. Handles errors.
 
+var command = "ls"; // Command here
+
 console.log("[run-shell-command] - attempting to run shell command...");
-    const command = exec(args.command);
+const command = exec(command);
 
-    command.stdout.on("data", data => {
-        console.log("[run-shell-command] - success. stdout: " + data);
-        event.returnValue = data;
-    });
+command.stdout.on("data", data => {
+    console.log("[run-shell-command] - success. stdout: " + data);
+    event.returnValue = data;
+});
 
-    command.stderr.on("data", data => {
-        console.log("[run-shell-command] - stderr: " + data);
-        event.returnValue = data;
-    });
+command.stderr.on("data", data => {
+    console.log("[run-shell-command] - stderr: " + data);
+    event.returnValue = data;
+});
 
-    command.on("error", (error) => {
-        console.log("[run-shell-command] - error: " + error.message);
-        event.returnValue = data;
-    });
+command.on("error", (error) => {
+    console.log("[run-shell-command] - error: " + error.message);
+    event.returnValue = data;
+});
 
-    command.on("close", code => {
-        console.log("[run-shell-command] - child process finished w/ code " + code);
-    });
+command.on("close", code => {
+    console.log("[run-shell-command] - child process finished w/ code " + code);
+});
